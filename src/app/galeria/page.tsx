feca20db7bdbd5fa7fddf1bb88fb39bbffbfa969
fhellipevalentin <fhellipereis@outlook.com>
2025-10-05 14:10:18 -0300

    Componente ImageCard e Pagina Galeria

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -1,9 +1,14 @@
 import { Template } from "@/components/Template";
+import { ImageCard } from "@/components/ImageCard";
 
 export default function GaleriaPage () {
     return (
         <Template>
-            <h1>Galeria</h1>
+            <section className="grid grid-cols-3 gap-8">
+                <ImageCard src="https://retornar.com.br/wp-content/uploads/2024/02/MGN04846-Sorteio-Jeep-Renegade.jpg" nome="Jeep Renegade" tamanho="10MB" dataUpload="2024-02-20" />
+                <ImageCard src="https://retornar.com.br/wp-content/uploads/2024/02/MGN04846-Sorteio-Jeep-Renegade.jpg" nome="Jeep Renegade" tamanho="10MB" dataUpload="2024-02-20" />
+                <ImageCard src="https://retornar.com.br/wp-content/uploads/2024/02/MGN04846-Sorteio-Jeep-Renegade.jpg" nome="Jeep Renegade" tamanho="10MB" dataUpload="2024-02-20" />
+            </section>
         </Template>
     )
 }
