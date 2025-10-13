7f23d0074261cfc9ab71853b4e9952af4bed4c98
fhellipevalentin <fhellipereis@outlook.com>
2025-10-13 11:07:59 -0300

    criando o formulario de pesquisa

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -33,7 +33,16 @@ export default function GaleriaPage () {
 
     return (
         <Template>
-            <button onClick={searchImages} className="bg-blue-500 text-white px-4 py-2 rounded mb-4">Mudar Imagem</button>          
+            <section className="flex flex-col items-center justify-center my-8">
+                <div className="flex space-x-4">
+                    <input type='text' placeholder="Buscar imagens..." className="border px-3 py-3 rounded-lg text-gray-900 " />
+                    <select className="border px-4 py-2 rounded-lg text-gray-900">
+                        <option>Todos os Formatos</option>
+                    </select>
+                    <button className="bg-blue-500 text-white px-4 py-2 rounded-lg" onClick={searchImages}>Search</button>
+                    <button className="bg-yellow-500 text-white px-4 py-2 rounded-lg">Add New</button>
+                </div>
+            </section>
             <section className="grid grid-cols-4 gap-8">
                 {
                     renderImageCards()
