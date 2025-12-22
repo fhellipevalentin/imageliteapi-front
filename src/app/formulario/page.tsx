07123b6bd2e1fe96d76862b6e6aba4ddce9d5564
fhellipevalentin <fhellipereis@outlook.com>
2025-12-22 11:15:29 -0300

    melhorias

--- src/app/formulario/page.tsx
+++ src/app/formulario/page.tsx
@@ -9,15 +9,15 @@ export default function FormularioPage() {
               <h5 className="mt-3  mb-10 text-3xl font-extrabold tracking-tight text-gray-900">Nova Imagem:</h5>    
                 <form className="w-full max-w-lg bg-white p-8 rounded-lg shadow-md">
                     <div className="grid grid-cols-1 gap-6 mb-6">
-                        <label className="block text-sm font-medium text-gray-700 mb-2">Nome da Imagem:</label>
+                        <label className="blockfont-high text-gray-700 mb-2">Nome da Imagem:</label>
                         <InputText placeholder= "digite o nome da imagem" style="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500" /> 
                     </div>
                     <div className="grid grid-cols-1 gap-6 mb-6">
-                        <label className="block text-sm font-medium text-gray-700 mb-2">Tags:</label>
+                        <label className="block font-medium text-gray-700 mb-2">Tags:</label>
                         <InputText placeholder= "digite as tags da imagem separadas por vírgula" style="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500" /> 
                     </div>
                     <div className="grid grid-cols-1 gap-6 mb-6">
-                        <label className="block text-sm font-medium text-gray-700 mb-2">Imagem:</label>
+                        <label className="block font-medium text-gray-700 mb-2">Imagem:</label>
                         <div className="flex items-center justify-center w-full">
                             <label className="flex flex-col border-4 border-dashed w-full h-32 hover:bg-gray-100 hover:border-blue-300 group">
                                 <div className="flex flex-col items-center justify-center pt-7">
