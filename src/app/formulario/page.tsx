94a87c81168d4f9e996131678ed0ae977041b0ca
fhellipevalentin <fhellipereis@outlook.com>
2025-12-23 11:12:38 -0300

    capturando valores dos campos com formik

--- src/app/formulario/page.tsx
+++ src/app/formulario/page.tsx
@@ -1,17 +1,39 @@
+'use client';
+
 import { Button } from "@/components/button/Button";
 import { InputText } from "@/components/input/InputText"
 import { Template } from "@/components/Template"
 import { ArrowLeftIcon } from '@heroicons/react/24/outline'; 
+import { useFormik } from "formik";
+
+
+interface FormProps {
+    nomeImagem: string;
+    tags: string;
+    imagem: File | null;
+}
 
 import Link from "next/link"
+import { use } from "react";
 
 export default function FormularioPage() {
+
+    const formik = useFormik<FormProps>({
+        initialValues: {
+            nomeImagem: "",
+            tags: "",
+            imagem: null
+        },
+        onSubmit: (values) => {
+            console.log(values);
+        }
+    });
     
     return (
         <Template>
             <section className="flex flex-col items-center justify-center my-8">
               <h5 className="mt-3  mb-10 text-3xl font-extrabold tracking-tight text-gray-900">Nova Imagem:</h5>    
-                <form className="w-full max-w-lg bg-white p-8 rounded-lg shadow-md">
+                <form  onSubmit={formik.handleSubmit} className="w-full max-w-lg bg-white p-8 rounded-lg shadow-md">
                     <div className="grid grid-cols-1 gap-6 mb-6">
                         <Link href="/galeria">
                             <Button type="button" style="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded mb-4 flex items-center">
@@ -21,7 +43,7 @@ export default function FormularioPage() {
                     </div>
                     <div className="grid grid-cols-1 gap-6 mb-6">
                         <label className="block font-medium  text-gray-700 mb-2">Nome da Imagem:</label>
-                        <InputText placeholder= "digite o nome da imagem" style="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500" /> 
+                        <InputText id="nomeImagem" onChange={formik.handleChange} placeholder="digite o nome da imagem" style="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500" /> 
                     </div>
                     <div className="grid grid-cols-1 gap-6 mb-6">
                         <label className="block font-medium text-gray-700 mb-2">Tags:</label>
--- src/components/input/InputText.tsx
+++ src/components/input/InputText.tsx
@@ -4,14 +4,16 @@ interface InputTextProps {
     onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
     style?: string;
     placeholder?: string;
+    id?: string;
 }
 
-export const InputText: React.FC<InputTextProps> = ({ onChange, style, placeholder }: InputTextProps) => {
+export const InputText: React.FC<InputTextProps> = ({ onChange, style, placeholder, id }: InputTextProps) => {
 
     return (
         <input type='text' 
                     onChange = {onChange}
                     placeholder={placeholder}
+                    id={id}
                     className={`border px-3 py-3 rounded-lg text-gray-900 ${style}`} />
     )
 
